502a1ed169525516b35ba8d89f469725ba327d9a
King-Key <guo_wang_113@163.com>
2019-01-06 23:39:48 +0800

    add

---
+++ Titanic/titanic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-01-06 22:01:34
+# @Author  : WangGuo
+# @Email   : guo_wang_113@163.com
+# @Github  : https://github.com/King-Key
+
+
+
